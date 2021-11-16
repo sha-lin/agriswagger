f28225a70b0d2101897d2f9a98d0d8e945848175
sha-lin <shalin.rono@student.moringaschool.com>
2021-11-16 09:53:53 +0300

    add corsheaders

--- agriproject/settings.py
+++ agriproject/settings.py
@@ -44,6 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'agri',
+    'rest_framework',
+    'drf_yasg',
+    'cloudinary',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +59,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost',
 ]
 
 ROOT_URLCONF = 'agriproject.urls'
